7a2ed7ddaf9830738dacf2bb11038ff59618a484
Juan Antonio Brena Moral <bren@juanantonio.info>
2022-03-29 17:22:21 +0200

    Initial demo with Spring

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# spring-boot-amadeus-sdk-demo
-Example using Spring Boot + Amadeus Java SDK
+# Spring Boot + Amadeus Java SDK Demo
+
+## Motivation
+
+Sometimes, I need to travel in Europe or cross the Atlantic ocean but
+after 2 years of COVID, before travelling I need to review country restrictions
+defined for travellers.
+
+Amadeus for developers provides an API for that and it offer information for 200 countries.
+
+## How to use it?
+
+Register in [Amadeus for Developers](https://developers.amadeus.com) to get your `AMADEUS_CLIENT_ID` & `AMADEUS_CLIENT_SECRET`
+
+Export the values:
+
+```
+export AMADEUS_CLIENT_ID=YOUR_CLIENT_ID
+export AMADEUS_CLIENT_SECRET=YOUR_CLIENT_SECRET
+```
+
+## How to run in local?
+
+```
+mvn spring-boot:run
+mvn clean package
+java -jar ./target/spring-boot-amadeus-sdk-demo-0.1.0-SNAPSHOT.jar
+```
+
+## How to run as a Docker Container?
+
+```
+mvn spring-boot:build-image
+docker run \
+    -e AMADEUS_CLIENT_ID=$AMADEUS_CLIENT_ID \
+    -e AMADEUS_CLIENT_SECRET=$AMADEUS_CLIENT_SECRET \
+    -t docker.io/library/spring-boot-amadeus-sdk-demo:0.1.0-SNAPSHOT
+```
+
+## Others commands
+
+```
+mvn prettier:write
+mvn versions:display-dependency-updates
+```
---
+++ src/main/java/com/example/demo/MainApplication.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.DiseaseAreaReport;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
+
+@Configuration
+class WebConfig {
+
+    public static Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
+    @Value("${AMADEUS_CLIENT_ID}")
+    private String AMADEUS_CLIENT_ID;
+
+    @Value("${AMADEUS_CLIENT_SECRET}")
+    private String AMADEUS_CLIENT_SECRET;
+
+    @PostConstruct
+    private void postConstruct() {
+        logger.info("Loading environment variables:");
+        logger.info("AMADEUS_CLIENT_ID: {}", AMADEUS_CLIENT_ID);
+        logger.info("AMADEUS_CLIENT_SECRET: {}", AMADEUS_CLIENT_SECRET);
+    }
+
+    @Bean
+    public Amadeus getAmadeusBean() {
+        return Amadeus.builder(AMADEUS_CLIENT_ID, AMADEUS_CLIENT_SECRET).build();
+    }
+}
+
+@RestController
+class WebController {
+
+    @Autowired
+    private Amadeus amadeus;
+
+    @GetMapping("/api/v1/covid-restrictions")
+    public ResponseEntity<String> getCOVIDRestrictions(@RequestParam String countryCode) throws ResponseException {
+        DiseaseAreaReport diseaseAreaReport = amadeus.dutyOfCare.diseases.covid19AreaReport.get(
+            Params.with("countryCode", countryCode)
+        ); //.and("cityCode", "MAD")
+
+        return ResponseEntity.ok().body("Hello World");
+    }
+}
